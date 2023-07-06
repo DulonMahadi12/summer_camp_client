a12bbfb0811d6862d7e4525264bbc5e5ebf80218
Dulon <dulonmolla88@gmail.com>
2023-07-06 20:07:04 +0600

    update

--- src/hooks/useAxiosIntercept.jsx
+++ src/hooks/useAxiosIntercept.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 const useAxiosIntercept = () => {
   //
   const axiosSecure = axios.create({
-    // baseURL: 'https://summer-camp-server-14hak.vercel.app',
     baseURL: 'https://summer-camp-server-14hak.vercel.app',
   });
   //
